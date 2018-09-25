b9c5c312d479c44cade8d3b92bba4dbcacaad201
poojay1 <poojay25101996@gmail.com>
2018-09-25 13:26:58 +0530

    Adding 4th option

--- mypage.php
+++ mypage.php
@@ -4,25 +4,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>query</h1>
-<form action="mypage.php" method="post">
-Enter Department Name <input type="text" name="deptname">
-<input type="submit" name="submit">
-</form>
+<h1>Employees database</h1>
 <?php
-	$a = 5;
-	//print_r(get_loaded_extensions());
-	//echo extension_loaded("mongodb") ? "loaded\n" : "not loaded\n";
-	//phpinfo();
-	//$mng = new MongoClient();
-	//$mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-	//$stats = new MongoDB\Driver\Command(["dbstats" => 1]);
-	//$res = $mng->executeCommand("employees", $stats);
-	//$query = new MongoDB\Driver\Query([]);
-	//$rows = $mng->executeQuery("employees.employees", $query);
-	//$stats = current($res->toArray());
-
-    	//print_r($stats);
 	$servername = "localhost";
 	$username = "root";
 	$password = "momnaninana";
@@ -31,32 +14,38 @@ Enter Department Name <input type="text" name="deptname">
 	$conn = mysqli_connect($servername, $username, $password, $db);
 	// Check connection
 	if (mysqli_connect_errno())
-  	{
-  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  	}
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 	$sql = 'SELECT * FROM departments';
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
+		echo "The various departments are <ul>"; 
 		while($row = $result->fetch_assoc()) {
-			echo $row["dept_name"],"\n";
+			echo "<li>", $row["dept_name"],"</li>";
 		}
+		echo "</ul>";
 	} else {
-		echo "0 results";
+		echo "Connection to database failed";
 	}
-	echo "Connected successfully";
-	echo "I AM ECHO";
+?>
+<form action="mypage.php" method="post">
+<h3> For getting information on employees in a department </h3>
+Enter Department Name <input type="text" name="deptname">
+<input type="submit" name="submit">
+</form>
+<form action="mypage.php" method="post">
+<h3> For getting gender ratio of a department </h3>
+Enter Department Name <input type="text" name="deptname">
+<input type="submit" name="submit1">
+</form>
+<?php
 	if(isset($_POST["submit"])){
-		echo "u submitted",$_POST["deptname"] ;
 		$sql = 'SELECT dept_no FROM departments WHERE dept_name="' . $_POST["deptname"] . '"';
-		echo $sql;
 		$result = $conn->query($sql);
-		//echo $result->fetch_assoc()["dept_no"];
 		$x = $result->fetch_assoc()["dept_no"];
-		echo $x;
 		if ($result->num_rows > 0) {
 			$sql = 'SELECT * FROM employees WHERE emp_no IN (SELECT emp_no FROM dept_emp WHERE dept_no="'. $x .'") ORDER BY hire_date'; 
-			//$sql = 'SELECT * FROM dept_emp WHERE dept_no="'. $x . '" ORDER BY from_date';
-			echo $sql;
 			$result = $conn->query($sql);
 			echo "<table><tr><th>Employee ID</th><th>Name</th><th>Start Date</th></tr>";
 			if ($result->num_rows > 0) {
@@ -68,11 +57,22 @@ Enter Department Name <input type="text" name="deptname">
 				echo "No employees in the department";
 			}
 		}else{
-			
 			echo "No such department";
 		}
 	}				
-	
+	if(isset($_POST["submit1"])){
+		$sql = 'SELECT dept_no FROM departments WHERE dept_name="' . $_POST["deptname"] . '"';
+		$result = $conn->query($sql);
+		$x = $result->fetch_assoc()["dept_no"];
+		if ($result->num_rows > 0) {
+			$sql = 'SELECT count(*) AS males FROM employees WHERE emp_no IN (SELECT emp_no FROM dept_emp WHERE dept_no="'. $x .'") AND gender="M"'; 
+			echo $sql;
+			$result = $conn->query($sql);
+			echo $result->fetch_assoc()["males"];
+		}else{
+			echo "No such department";
+		}
+	}	
 	$conn->close();
 ?>
 
